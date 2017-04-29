23dde6465f943636a10a47a2e1fa8be030aab7ff
silverkan <icyhins@gmail.com>
2017-04-29 06:10:29 -0400

    Project Artitecture Enhancement.

---
+++ food-delivery-service-member/src/main/java/silver/bittiger/cs504/food/member/FoodDeliveryMemberServiceApplication.java
@@ -0,0 +1,15 @@
+package silver.bittiger.cs504.food.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vagrant on 4/29/17.
+ */
+@SpringBootApplication
+public class FoodDeliveryMemberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryMemberServiceApplication.class, args);
+    }
+}
---
+++ food-delivery-service-parent/src/main/java/FoodDeliveryServiceApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vagrant on 4/29/17.
+ */
+@SpringBootApplication
+public class FoodDeliveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryServiceApplication.class, args);
+    }
+}
